c8d87433aff7df61406e0a3d410da4514b5cc890
lymao <lymaodt@gmail.com>
2016-07-27 23:00:31 +0700

    add unitest for repository and service

---
+++ Shop1.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shop1.Data.Infrastructure;
+using Shop1.Data.Repositories;
+using Shop1.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop1.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        private IDbFactory dbFactory;
+        private IPostCategoryRepository objRepository;
+        private IUnitOfWork unitOfWork;
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepository.GetAll().ToList();
+            Assert.AreEqual(4, list.Count);
+        }
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test Category";
+            category.Alias = "Test-Category";
+            category.Status = true;
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.ID);
+        }
+    }
+}
