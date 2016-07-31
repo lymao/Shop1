6d548c7870120ddfc043ad51f6abdf7d7fdc0e19
lymao <lymaodt@gmail.com>
2016-07-31 22:46:04 +0700

    add automapper

--- Shop1.Web/Api/PostCategoryController.cs
+++ Shop1.Web/Api/PostCategoryController.cs
@@ -1,6 +1,9 @@
-﻿using Shop1.Model.Models;
+﻿using AutoMapper;
+using Shop1.Model.Models;
 using Shop1.Service;
 using Shop1.Web.Infrastructure.Core;
+using Shop1.Web.Infrastructure.Extentions;
+using Shop1.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,28 +17,27 @@ namespace Shop1.Web.Api
     public class PostCategoryController : ApiControllerBase
     {
         IPostCategoryService _postCategoryService;
-
-        public PostCategoryController(IErrorService errorService, IPostCategoryService postCategoryService) :
-            base(errorService)
+        public PostCategoryController(IErrorService errorService, IPostCategoryService postCategoryService) : base(errorService)
         {
             this._postCategoryService = postCategoryService;
         }
-
         [Route("getall")]
         public HttpResponseMessage Get(HttpRequestMessage request)
         {
             return CreateHttpResponse(request, () =>
             {
-                var listCategory = _postCategoryService.GetAll();
+                var listPostCategory = _postCategoryService.GetAll();
+                var listPostCategoryVm = Mapper.Map<List<PostCategoryViewModel>>(listPostCategory);
 
-                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listCategory);
-
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listPostCategoryVm);
 
                 return response;
             });
-        }
 
-        public HttpResponseMessage Post(HttpRequestMessage request, PostCategory postCategory)
+        }
+        [Route("add")]
+
+        public HttpResponseMessage Post(HttpRequestMessage request, PostCategoryViewModel postCategoryVm)
         {
             return CreateHttpResponse(request, () =>
             {
@@ -46,17 +48,18 @@ namespace Shop1.Web.Api
                 }
                 else
                 {
-                    var category = _postCategoryService.Add(postCategory);
+                    PostCategory newPostCategory = new PostCategory();
+                    newPostCategory.UpdatePostCategory(postCategoryVm);
+                    var category = _postCategoryService.Add(newPostCategory);
                     _postCategoryService.Save();
-
                     response = request.CreateResponse(HttpStatusCode.Created, category);
-
                 }
                 return response;
             });
-        }
 
-        public HttpResponseMessage Put(HttpRequestMessage request, PostCategory postCategory)
+        }
+        [Route("update")]
+        public HttpResponseMessage Put(HttpRequestMessage request, PostCategoryViewModel postCategoryVm)
         {
             return CreateHttpResponse(request, () =>
             {
@@ -67,16 +70,16 @@ namespace Shop1.Web.Api
                 }
                 else
                 {
-                    _postCategoryService.Update(postCategory);
+                    var postCategoryDb = _postCategoryService.GetById(postCategoryVm.ID);
+                    postCategoryDb.UpdatePostCategory(postCategoryVm);
+                    _postCategoryService.Update(postCategoryDb);
                     _postCategoryService.Save();
-
                     response = request.CreateResponse(HttpStatusCode.OK);
-
                 }
                 return response;
             });
-        }
 
+        }
         public HttpResponseMessage Delete(HttpRequestMessage request, int id)
         {
             return CreateHttpResponse(request, () =>
@@ -90,12 +93,11 @@ namespace Shop1.Web.Api
                 {
                     _postCategoryService.Delete(id);
                     _postCategoryService.Save();
-
                     response = request.CreateResponse(HttpStatusCode.OK);
-
                 }
                 return response;
             });
+
         }
     }
 }
---
+++ Shop1.Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Shop1.Model.Models;
+using Shop1.Web.Models;
+
+namespace Shop1.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.CreateMap<Post, PostViewModel>();
+            Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
+            Mapper.CreateMap<Tag, TagViewModel>();
+        }
+    }
+}
