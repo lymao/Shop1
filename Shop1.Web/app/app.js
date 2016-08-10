1df5dfc5f23d27b6d87fb7d7f5c5e0d70463a050
lymao <lymaodt@gmail.com>
2016-08-10 22:03:36 +0700

    commit bai22

--- Shop1.Web/app/app.js
+++ Shop1.Web/app/app.js
@@ -1,1 +1,15 @@
-﻿
+﻿/// <reference path="D:\C#\ItClass\AspNetApi_AngularJs\Projects\Git1\Shop1.Web\Assets/admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module("shop1", ["shop1.products", "shop1.common"]).config(config);
+    config.$inject = ["$stateProvider", "$urlRouterProvider"];
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state("home", {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+
+    }
+})();
