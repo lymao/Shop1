eea1b6f70362c32e1ff5a01f7307a7f86a5cd50c
DESKTOP-VK24Q3F\lymao <lymaodt@gmail.com>
2016-09-10 00:11:01 +0700

    productCategoryEditController.js, commonService.js,

--- Shop1.Web/app/shared/services/apiService.js
+++ Shop1.Web/app/shared/services/apiService.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
+﻿/// <reference path="D:\C#\ItClass\AspNetApi_AngularJs\Projects\Git\Shop.Web\Assets/admin/libs/angular/angular.js" />
 
 (function (app) {
     app.factory('apiService', apiService);
@@ -8,7 +8,24 @@
     function apiService($http, notificationService) {
         return {
             get: get,
-            post: post
+            post: post,
+            put: put,
+            del: del
+        }
+
+        function del(url, data, success, failure) {
+            $http.delete(url, data).then(function (result) {
+                success(result);
+            }, function (error) {
+                console.log(error.status)
+                if (error.status === 401) {
+                    notificationService.displayError('Authenticate is required.');
+                }
+                else if (failure != null) {
+                    failure(error);
+                }
+
+            });
         }
 
         function post(url, data, success, failure) {
@@ -25,6 +42,22 @@
 
             });
         }
+
+        function put(url, data, success, failure) {
+            $http.put(url, data).then(function (result) {
+                success(result);
+            }, function (error) {
+                console.log(error.status)
+                if (error.status === 401) {
+                    notificationService.displayError('Authenticate is required.');
+                }
+                else if (failure != null) {
+                    failure(error);
+                }
+
+            });
+        }
+
         function get(url, params, success, failure) {
             $http.get(url, params).then(function (result) {
                 success(result);
