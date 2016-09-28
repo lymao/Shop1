dfe747955248b54f5808f9cd35727f2fad437b88
DESKTOP-VK24Q3F\lymao <lymaodt@gmail.com>
2016-09-28 23:01:43 +0700

    add login form

--- Shop1.Web/app/app.js
+++ Shop1.Web/app/app.js
@@ -4,12 +4,22 @@
     angular.module("shop1", ["shop1.products","shop1.product_categories", "shop1.common"]).config(config);
     config.$inject = ["$stateProvider", "$urlRouterProvider"];
     function config($stateProvider, $urlRouterProvider) {
-        $stateProvider.state("home", {
-            url: "/admin",
-            templateUrl: "/app/components/home/homeView.html",
-            controller: "homeController"
-        });
-        $urlRouterProvider.otherwise('/admin');
-
+        $stateProvider
+            .state('base', {
+                url: '',
+                templateUrl: '/app/shared/views/baseView.html',
+                abstract: true
+            }).state('login', {
+                url: "/login",
+                templateUrl: "/app/components/login/loginView.html",
+                controller: "loginController"
+            })
+            .state('home', {
+                url: "/admin",
+                parent: 'base',
+                templateUrl: "/app/components/home/homeView.html",
+                controller: "homeController"
+            });
+        $urlRouterProvider.otherwise('/login');
     }
 })();
---
+++ Shop1.Web/app/components/login/loginController.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="D:\C#\ItClass\AspNetApi_AngularJs\Projects\Git1\Shop1.Web\Assets/admin/libs/angular/angular.js" />
+(function (app) {
+    app.controller('loginController', loginController);
+    loginController.$inject = ['$scope', '$state'];
+    function loginController($scope, $state) {
+        $scope.loginSubmit = function () {
+            $state.go('home');
+        }
+    }
+})(angular.module('shop1'));
