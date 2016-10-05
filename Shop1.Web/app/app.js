bc7a5302e346d198500195570ca35e8e6f9f8142
DESKTOP-VK24Q3F\lymao <lymaodt@gmail.com>
2016-10-05 21:44:56 +0700

    login with admin

--- Shop1.Web/app/app.js
+++ Shop1.Web/app/app.js
@@ -1,7 +1,9 @@
 ﻿/// <reference path="D:\C#\ItClass\AspNetApi_AngularJs\Projects\Git1\Shop1.Web\Assets/admin/libs/angular/angular.js" />
 
 (function () {
-    angular.module("shop1", ["shop1.products","shop1.product_categories", "shop1.common"]).config(config);
+    angular.module("shop1", ["shop1.products", "shop1.product_categories", "shop1.common"])
+        .config(config)
+        .config(configAuthentication);
     config.$inject = ["$stateProvider", "$urlRouterProvider"];
     function config($stateProvider, $urlRouterProvider) {
         $stateProvider
@@ -21,5 +23,33 @@
                 controller: "homeController"
             });
         $urlRouterProvider.otherwise('/login');
+    }
+    function configAuthentication($httpProvider) {
+        $httpProvider.interceptors.push(function ($q, $location) {
+            return {
+                request: function (config) {
+
+                    return config;
+                },
+                requestError: function (rejection) {
+
+                    return $q.reject(rejection);
+                },
+                response: function (response) {
+                    if (response.status == "401") {
+                        $location.path('/login');
+                    }
+                    //the same response/modified/or a new one need to be returned.
+                    return response;
+                },
+                responseError: function (rejection) {
+
+                    if (rejection.status == "401") {
+                        $location.path('/login');
+                    }
+                    return $q.reject(rejection);
+                }
+            };
+        });
     }
 })();
