2b23f2d847d16fbf0043311cdb590143104410c1
DESKTOP-VK24Q3F\lymao <lymaodt@gmail.com>
2016-11-05 21:36:42 +0700

    send email and captcha

---
+++ Shop1.Service/FeedbackService.cs
@@ -0,0 +1,35 @@
+﻿using Shop1.Data.Infrastructure;
+using Shop1.Data.Repositories;
+using Shop1.Model.Models;
+
+namespace Shop1.Service
+{
+    public interface IFeedbackService
+    {
+        Feedback Create(Feedback feedback);
+
+        void Save();
+    }
+
+    public class FeedbackService : IFeedbackService
+    {
+        private IFeedbackRepository _feedbackRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public FeedbackService(IFeedbackRepository feedbackRepository, IUnitOfWork unitOfWork)
+        {
+            _feedbackRepository = feedbackRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Feedback Create(Feedback feedback)
+        {
+            return _feedbackRepository.Add(feedback);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+    }
+}
--- Shop1.Web/Controllers/ContactController.cs
+++ Shop1.Web/Controllers/ContactController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using BotDetect.Web.Mvc;
+using Shop1.Common;
 using Shop1.Model.Models;
 using Shop1.Service;
+using Shop1.Web.Infrastructure.Extentions;
 using Shop1.Web.Models;
 using System;
 using System.Collections.Generic;
@@ -14,15 +17,49 @@ namespace Shop1.Web.Controllers
     {
         // GET: Contact
         IContactDetailService _contactDetailService;
-        public ContactController(IContactDetailService contactDetailService)
+        IFeedbackService _feedbackService;
+        public ContactController(IContactDetailService contactDetailService, IFeedbackService feedbackService)
         {
             this._contactDetailService = contactDetailService;
+            this._feedbackService = feedbackService;
         }
         public ActionResult Index()
         {
+            FeedbackViewModel viewModel = new FeedbackViewModel();
+            viewModel.ContactDetail = GetDetail();
+            return View(viewModel);
+        }
+        [HttpPost]
+        [CaptchaValidation("CaptchaCode", "contactCaptcha", "Mã xác nhận không đúng")]
+        public ActionResult SendFeedback(FeedbackViewModel feedbackViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Feedback newFeedback = new Feedback();
+                newFeedback.UpdateFeedback(feedbackViewModel);
+                _feedbackService.Create(newFeedback);
+                _feedbackService.Save();
+
+                ViewData["SuccessMsg"] = "Gửi phản hồi thành công";
+
+                string content = System.IO.File.ReadAllText(Server.MapPath("/Assets/client/template/contact_template.html"));
+                content = content.Replace("{{Name}}", feedbackViewModel.Name);
+                content = content.Replace("{{Email}}", feedbackViewModel.Email);
+                content = content.Replace("{{Message}}", feedbackViewModel.Message);
+                var adminEmail = ConfigHelper.GetByKey("AdminEmail");
+                MailHelper.SendMail(adminEmail, "Thông tin liên hệ từ website", content);
+                feedbackViewModel.Name = "";
+                feedbackViewModel.Message = "";
+                feedbackViewModel.Email = "";
+            }
+            feedbackViewModel.ContactDetail = GetDetail();
+            return View("Index", feedbackViewModel);
+        }
+        private ContactDetailViewModel GetDetail()
+        {
             var model = _contactDetailService.GetDefaultContact();
             var contactViewModel = Mapper.Map<ContactDetail, ContactDetailViewModel>(model);
-            return View(contactViewModel);
+            return contactViewModel;
         }
     }
 }
